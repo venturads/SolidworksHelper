302b0df403dd55e48ba45361c2351008ea78b0b6
venturads <albert@venturads.com>
2020-05-27 15:25:25 -0700

    updated menu and images

--- www/script.js
+++ www/script.js
@@ -87,127 +87,127 @@ document.getElementById("root").innerHTML = "<div class='loader'></div>";
   let data=[
     {
       id: 2020,
-      title: "Chart",
+      title: "Box Extrude1",
       content: "Welding Chart",
       url: "img/1.jpg",
       urlToImage: "img/1.jpg"
     },{
       id: 2021,
-      title: "Symbols",
+      title: "Box Extrude2",
       content: "Symbols",
       url: "img/2.jpg",
       urlToImage: "img/2.jpg"
     },{
       id: 2022,
-      title: "Specs",
+      title: "Box Extrude3",
       content: "Specs",
       url: "img/3.jpg",
       urlToImage: "img/3.jpg"
     },{
       id: 2023,
-      title: "Settings",
+      title: "Hole Pattern1",
       content: "Settings",
       url: "img/4.jpg",
       urlToImage: "img/4.jpg"
     },{
       id: 2024,
-      title: "Basic Symbols",
+      title: "Hole Pattern2",
       content: "Basic Symbols",
       url: "img/5.jpg",
       urlToImage: "img/5.jpg"
     },  {
       id: 2025,
-      title: "Chart",
+      title: "Hole Pattern3",
       content: "Welding Chart",
       url: "img/6.jpg",
       urlToImage: "img/6.jpg"
     },{
       id: 2026,
-      title: "Data",
+      title: "Hole Pattern4",
       content: "Data",
       url: "img/7.jpg",
       urlToImage: "img/7.jpg"
     },{
       id: 2027,
-      title: "Types",
+      title: "Hole Pattern5",
       content: "Types",
       url: "img/8.jpg",
       urlToImage: "img/8.jpg"
     },{
       id: 2028,
-      title: "More Types",
+      title: "Allen Key1",
       content: "More Types",
       url: "img/9.jpg",
       urlToImage: "img/9.jpg"
     },{
       id: 2029,
-      title: "Weld Joints",
+      title: "Allen Key2",
       content: "Weld Joints",
       url: "img/10.jpg",
       urlToImage: "img/10.jpg"
     },{
       id: 2030,
-      title: "Process",
+      title: "Allen Key3",
       content: "Process",
       url: "img/11.jpg",
       urlToImage: "img/11.jpg"
     },{
       id: 2031,
-      title: "Safety",
+      title: "Allen Key4",
       content: "Safety",
       url: "img/12.jpg",
       urlToImage: "img/12.jpg"
     },{
       id: 2032,
-      title: "Equipment",
+      title: "Sheet Metal1",
       content: "Equipment",
       url: "img/13.jpg",
       urlToImage: "img/13.jpg"
     },{
       id: 2033,
-      title: "Workbench",
+      title: "Sheet Metal2",
       content: "Workbench",
       url: "img/14.jpg",
       urlToImage: "img/14.jpg"
     },{
       id: 2034,
-      title: "Seated Workbench",
+      title: "Sheet Metal3",
       content: "Seated Workbench",
       url: "img/15.jpg",
       urlToImage: "img/15.jpg"
     },  {
       id: 2035,
-      title: "Mill Direction",
+      title: "Sheet Metal4",
       content: "Mill Direction",
       url: "img/16.jpg",
       urlToImage: "img/16.jpg"
     },{
       id: 2036,
-      title: "Cutting Speeds",
+      title: "Sheet Metal5",
       content: "Cutting Speeds",
       url: "img/17.jpg",
       urlToImage: "img/17.jpg"
     },{
       id: 2037,
-      title: "Decimal Chart",
+      title: "Spring Spiral1",
       content: "Decimal Chart",
       url: "img/18.jpg",
       urlToImage: "img/18.jpg"
     },{
       id: 2038,
-      title: "Rpm",
+      title: "Spring Spiral2",
       content: "Rpm",
       url: "img/19.jpg",
       urlToImage: "img/19.jpg"
     },{
       id: 2039,
-      title: "Tap/Drill Chart",
+      title: "Spring Spiral3",
       content: "Tap/Drill Chart",
       url: "img/20.jpg",
       urlToImage: "img/20.jpg"
     },{
       id: 2040,
-      title: "Inch/Metric Chart",
+      title: "Spring Spiral4",
       content: "Inch/Metric Chart",
       url: "img/21.jpg",
       urlToImage: "img/21.jpg"
@@ -219,16 +219,46 @@ document.getElementById("root").innerHTML = "<div class='loader'></div>";
       urlToImage: "img/22.jpg"
     },{
       id: 2042,
-      title: "Weld Details",
+      title: "Mating1",
       content: "Weld Details",
       url: "img/23.jpg",
       urlToImage: "img/23.jpg"
     },{
       id: 2043,
-      title: "Metal Inert Gas",
+      title: "Mating2",
       content: "Metal Inert Gas",
       url: "img/24.jpg",
       urlToImage: "img/24.jpg"
+    },{
+      id: 2034,
+      title: "Mating3",
+      content: "Seated Workbench",
+      url: "img/25.jpg",
+      urlToImage: "img/25.jpg"
+    },  {
+      id: 2035,
+      title: "Shell Cavity",
+      content: "Mill Direction",
+      url: "img/26.jpg",
+      urlToImage: "img/26.jpg"
+    },{
+      id: 2036,
+      title: "Centroid",
+      content: "Cutting Speeds",
+      url: "img/27.jpg",
+      urlToImage: "img/27.jpg"
+    },{
+      id: 2037,
+      title: "Hot Keys",
+      content: "Decimal Chart",
+      url: "img/28.jpg",
+      urlToImage: "img/28.jpg"
+    },{
+      id: 2038,
+      title: "Insert Image",
+      content: "Rpm",
+      url: "img/29.jpg",
+      urlToImage: "img/29.jpg"
     }
   ];
   let list = "";
