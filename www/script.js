e2a72ad673be58f5d9bb0a35845a79342b8e3820
venturads <albert@venturads.com>
2020-05-27 15:03:10 -0700

    added img to app and removed news tab and search

--- www/script.js
+++ www/script.js
@@ -4,7 +4,7 @@ const getSearch = function(){
   document.getElementById("root").innerHTML = "Search: " + searchVar;
   getSe(searchVar);
 }
-document.getElementById("btn").addEventListener("click", getSearch);
+// document.getElementById("btn").addEventListener("click", getSearch);
 function getSe (n){
   const se = 'http://venturads.com/weldinghelper/api/get_search_results/?search=' + n;
 
@@ -27,7 +27,7 @@ function getdata(data){
 }
   
 }
-document.getElementById('btn').addEventListener('click', getSe);
+// document.getElementById('btn').addEventListener('click', getSe);
 
 // get wordpress api json data
 function getNews (){
@@ -52,11 +52,12 @@ function getdata(data){
 }
   
 }
-document.getElementById('content-news').addEventListener('click', getNews);
+// document.getElementById('content-news').addEventListener('click', getNews);
 
 // get wordpress api json data
 function getGar (){
-    const n2 = 'http://venturads.com/weldinghelper/api/get_category_posts/?slug=solidworks';
+    //const n2 = 'http://venturads.com/weldinghelper/api/get_category_posts/?slug=welding';
+    const n2 = 'https://venturads.com/json/weld.json';
 
   document.getElementById("root").innerHTML = "<div class='loader'></div>";
     // alert("loading...");
@@ -70,14 +71,176 @@ function getGar (){
   
   function getdata(data){
     let list="";
-    for(let i=0;i<data.posts.length;i++){
-      list += "<div class='btn-group-justified content'><a  type='button' class='btn btn-info sharp' id='settings' data-toggle='collapse' data-target='#demo" + data.posts[i].id + "'>" +  data.posts[i].title + "</a></div><div id='demo" +  data.posts[i].id + "' class='collapse'>" + data.posts[i].content + "</div></div>";
+    for(let i=0;i<data.data.length;i++){
+      list += "<div class='btn-group-justified content'><a  type='button' class='btn btn-info sharp' id='settings' data-toggle='collapse' data-target='#demo" + data.data[i].id + "'>" +  data.data[i].title + "</a></div><div id='demo" +  data.data[i].id + "' class='collapse'>" + data.data[i].content + "</div></div>";
       }
     document.getElementById("root").innerHTML = list;
   }
     
   }
-  document.getElementById('content-app').addEventListener('click', getGar);
+
+// more menu list
+function getData (){
+  // const n2 = 'https://venturads.com/helper-menu.json';
+
+document.getElementById("root").innerHTML = "<div class='loader'></div>";
+  let data=[
+    {
+      id: 2020,
+      title: "Chart",
+      content: "Welding Chart",
+      url: "img/1.jpg",
+      urlToImage: "img/1.jpg"
+    },{
+      id: 2021,
+      title: "Symbols",
+      content: "Symbols",
+      url: "img/2.jpg",
+      urlToImage: "img/2.jpg"
+    },{
+      id: 2022,
+      title: "Specs",
+      content: "Specs",
+      url: "img/3.jpg",
+      urlToImage: "img/3.jpg"
+    },{
+      id: 2023,
+      title: "Settings",
+      content: "Settings",
+      url: "img/4.jpg",
+      urlToImage: "img/4.jpg"
+    },{
+      id: 2024,
+      title: "Basic Symbols",
+      content: "Basic Symbols",
+      url: "img/5.jpg",
+      urlToImage: "img/5.jpg"
+    },  {
+      id: 2025,
+      title: "Chart",
+      content: "Welding Chart",
+      url: "img/6.jpg",
+      urlToImage: "img/6.jpg"
+    },{
+      id: 2026,
+      title: "Data",
+      content: "Data",
+      url: "img/7.jpg",
+      urlToImage: "img/7.jpg"
+    },{
+      id: 2027,
+      title: "Types",
+      content: "Types",
+      url: "img/8.jpg",
+      urlToImage: "img/8.jpg"
+    },{
+      id: 2028,
+      title: "More Types",
+      content: "More Types",
+      url: "img/9.jpg",
+      urlToImage: "img/9.jpg"
+    },{
+      id: 2029,
+      title: "Weld Joints",
+      content: "Weld Joints",
+      url: "img/10.jpg",
+      urlToImage: "img/10.jpg"
+    },{
+      id: 2030,
+      title: "Process",
+      content: "Process",
+      url: "img/11.jpg",
+      urlToImage: "img/11.jpg"
+    },{
+      id: 2031,
+      title: "Safety",
+      content: "Safety",
+      url: "img/12.jpg",
+      urlToImage: "img/12.jpg"
+    },{
+      id: 2032,
+      title: "Equipment",
+      content: "Equipment",
+      url: "img/13.jpg",
+      urlToImage: "img/13.jpg"
+    },{
+      id: 2033,
+      title: "Workbench",
+      content: "Workbench",
+      url: "img/14.jpg",
+      urlToImage: "img/14.jpg"
+    },{
+      id: 2034,
+      title: "Seated Workbench",
+      content: "Seated Workbench",
+      url: "img/15.jpg",
+      urlToImage: "img/15.jpg"
+    },  {
+      id: 2035,
+      title: "Mill Direction",
+      content: "Mill Direction",
+      url: "img/16.jpg",
+      urlToImage: "img/16.jpg"
+    },{
+      id: 2036,
+      title: "Cutting Speeds",
+      content: "Cutting Speeds",
+      url: "img/17.jpg",
+      urlToImage: "img/17.jpg"
+    },{
+      id: 2037,
+      title: "Decimal Chart",
+      content: "Decimal Chart",
+      url: "img/18.jpg",
+      urlToImage: "img/18.jpg"
+    },{
+      id: 2038,
+      title: "Rpm",
+      content: "Rpm",
+      url: "img/19.jpg",
+      urlToImage: "img/19.jpg"
+    },{
+      id: 2039,
+      title: "Tap/Drill Chart",
+      content: "Tap/Drill Chart",
+      url: "img/20.jpg",
+      urlToImage: "img/20.jpg"
+    },{
+      id: 2040,
+      title: "Inch/Metric Chart",
+      content: "Inch/Metric Chart",
+      url: "img/21.jpg",
+      urlToImage: "img/21.jpg"
+    },{
+      id: 2041,
+      title: "Sheet Metal Charts",
+      content: "Sheet Metal Charts",
+      url: "img/22.jpg",
+      urlToImage: "img/22.jpg"
+    },{
+      id: 2042,
+      title: "Weld Details",
+      content: "Weld Details",
+      url: "img/23.jpg",
+      urlToImage: "img/23.jpg"
+    },{
+      id: 2043,
+      title: "Metal Inert Gas",
+      content: "Metal Inert Gas",
+      url: "img/24.jpg",
+      urlToImage: "img/24.jpg"
+    }
+  ];
+  let list = "";
+  console.log("mylist: " + data[0].title);
+  for(let i=0;i<data.length;i++){
+    list += "<div class='btn-group-justified content'><a  type='button' class='btn btn-info sharp' id='settings' data-toggle='collapse' data-target='#demo" + data[i].id + "'>" +  data[i].title + "</a></div><div id='demo" +  data[i].id + "' class='collapse'><img src='" + data[i].url + "' width='100%' ></div></div>";
+  document.getElementById('root').innerHTML = list;
+}
+  
+}
+
+document.getElementById('content-app').addEventListener('click', getData);
   
 // more menu list
   function getMenu (){
@@ -86,30 +249,26 @@ function getGar (){
   document.getElementById("root").innerHTML = "<div class='loader'></div>";
     let data=[
       {
-        id: 1, name: "MIG Weld Helper", link: "https://play.google.com/store/apps/details?id=com.venturads.LearnHowToMigWeld"
+        id: 1, name: "RPM Helper", link: "https://play.google.com/store/apps/details?id=com.venturads.RPM_HELPER"
       },
       {
-        id: 2, name: "TIG Weld Helper", link: "https://play.google.com/store/apps/details?id=com.venturads.LearnHowToTIGWeld"
+        id: 2, name: "Endline puzzle game", link: "https://play.google.com/store/apps/details?id=com.venturads.games.EndLine"
       },
       {
         id: 3, name: "CNC Helper", link: "https://play.google.com/store/apps/details?id=com.venturads.CNCHelperPlay"
       },
       {
-        id: 4, name: "Welding Helper", link: "https://play.google.com/store/apps/details?id=com.venturads.albert.SolidworksHelper"
+        id: 4, name: "Solidworks Helper", link: "https://play.google.com/store/apps/details?id=com.venturads.albert.SolidworksHelper"
       },
       {
-        id: 5, name: "Ring Helper", link: "https://play.google.com/store/apps/details?id=com.venturads.RingJumper"
+        id: 5, name: "Ballon Pop Free puzzle game", link: "https://play.google.com/store/apps/details?id=com.venturads.BalloonPopFree"
       },
       {
         id: 6, name: "Sheet Metal Helper", link: "https://play.google.com/store/apps/details?id=com.venturads.SheetMetalHelper"
       },
       {
-        id: 7, name: "Venturads.com", link: "https://venturads.com"
-      },
-      {
-        id: 8, name: "Privacy Policy", link: "http://venturads.com/pp.html"
-      },
-      
+        id: 6, name: "Venturads.com", link: "https://venturads.com"
+      }
     ];
     let list = "";
     console.log("mylist: " + data[0].name);
